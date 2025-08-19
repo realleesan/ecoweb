a18e3c2042aeaf7f0aa5542731880ac4594e37b2
trangtran83 <thuytrang832005@gmail.com>
2025-08-19 19:07:24 +0700

    bai 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tài liệu 1</title>
+</head>
+<body>
+
+    <?php
+    $name = "Trang";
+    echo "<h1>Xin chào, $name!</h1>";
+    ?>
+</body>
+</html>
